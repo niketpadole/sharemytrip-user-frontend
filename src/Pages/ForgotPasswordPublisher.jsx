c8115a53d4caf3f08c095360139a7bf4c40a454c
Niket Padole <138977504+niketpadole@users.noreply.github.com>
2024-06-29 00:21:29 +0530

    Add files via upload

--- src/Pages/ForgotPasswordPublisher.jsx
+++ src/Pages/ForgotPasswordPublisher.jsx
@@ -222,7 +222,7 @@ const ForgotPasswordPublisher = () => {
                   <div>
                     <label
                       htmlFor="confirmPassword"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-gray-900"
                     >
                       Confirm Password
                     </label>
@@ -232,7 +232,7 @@ const ForgotPasswordPublisher = () => {
                       id="confirmPassword"
                       value={confirmPassword}
                       onChange={(e) => setConfirmPassword(e.target.value)}
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5"
                       required
                     />
                   </div>
@@ -241,7 +241,7 @@ const ForgotPasswordPublisher = () => {
                   )}
                   <button
                     type="submit"
-                    className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                    className="w-full text-white bg-[#ff6f61] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
                   >
                     Reset Password
                   </button>
