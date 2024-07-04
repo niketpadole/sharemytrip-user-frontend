515f7b6be678de144b994ddb825fb51abf2fa689
ajinkya1209 <137904903+ajinkya1209@users.noreply.github.com>
2024-07-05 01:32:21 +0530

    Update PassangerLogin.jsx

--- src/Pages/PassangerLogin.jsx
+++ src/Pages/PassangerLogin.jsx
@@ -34,7 +34,7 @@ const Login = () => {
       }
 
     try {
-      const response = await axios.post(`http://13.201.203.99:8095/user/passengers/login`, {
+      const response = await axios.post(`https://api.sharemytrip.xyz/user/passengers/login`, {
         email: userEmail,
         password
       });
