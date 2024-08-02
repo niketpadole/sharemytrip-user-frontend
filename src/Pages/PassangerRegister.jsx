cf929c7b7240b959a97c630c9f3e5d159b76469d
Rohith Kumar Raju <134221673+crkr004@users.noreply.github.com>
2024-08-02 19:25:18 +0530

    Update PassangerRegister.jsx

--- src/Pages/PassangerRegister.jsx
+++ src/Pages/PassangerRegister.jsx
@@ -97,60 +97,7 @@ const PassangerRegister = () => {
     return isValid;
   };
 
-  // const register = async (e) => {
-  //   try {
-  //     e.preventDefault();
-  //     const validation = validate();
-  //     if (validation) {
-  //       const response = await axios.post(
-  //         "https://api.sharemytrip.xyz/user/passengers/register",
-  //         {
-  //           firstName,
-  //           lastName,
-  //           mobile,
-  //           email,
-  //           password,
-  //           dateOfBirth,
-  //           "aadharCard": aadhar,
-  //           miniBio,
-  //         }
-  //       );
-  // //       if (response.status === 201) {
-  // //         toast.success("Passenger Registration Successful", {
-  // //           duration: 3000,
-  // //         });
-  // //         navigate("/log-in/passanger");
-  // //       } else {
-  // //         toast.error(error.response.data, {
-  // //           duration: 3000
-  // //         });
-  // //       }
-  // //     } else {
-  // //       toast.error("Validation Failed");
-  // //     }
-  // //   } catch (error) {
-  // //     console.log(error);
-  // //     toast.error(error.response.data, {
-  // //       duration: 3000
-  // //     });
-  // //   }
-  // // };
-  //       if (response.status === 201) {
-  //         toast.success("Passenger Registration Successful", {
-  //           duration: 3000,
-  //         });
-  //         navigate("/log-in/passanger");
-  //       } else {
-  //         toast.error("Registration Failed");
-  //       }
-  //     } else {
-  //       console.log("Validation Failed");
-  //     }
-  //   } catch (error) {
-  //     console.log(error);
-  //     toast.error(error.response.data);
-  //   }
-  // };
+  
   const register = async (e) => {
     try {
       e.preventDefault();
@@ -182,9 +129,13 @@ const PassangerRegister = () => {
       }
     } catch (error) {
       console.log(error);
+      if(error.response.status===409){
         // toast.error("Email or Aadhaar already registered");
         toast.error(error.response.data);
-     
+      }
+      else{
+      toast.error("Server Error");
+      }
     }
   };
 
